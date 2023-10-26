dd4f94acc69698a585d46b634d7f8c0bf5d8bdb7
Brian Ho <YWHo@users.noreply.github.com>
2023-10-26 18:36:14 +1300

    using URL Constructor for relative paths

--- src/pluggins/unpkg-path-pluggin.ts
+++ src/pluggins/unpkg-path-pluggin.ts
@@ -9,9 +9,19 @@ export const unpkgPathPlugin = () => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a ' };
-        } else if (args.path === 'tiny-test-pkg') {
-          return { path: 'https://www.unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a' }
         }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, `${args.importer}/`).href
+          }
+        }
+
+        return {
+          namespace: 'a',
+          path: `https://www.unpkg.com/${args.path}`
+        };
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -22,7 +32,7 @@ export const unpkgPathPlugin = () => {
             loader: 'jsx',
             contents: `
               //import message from 'tiny-test-pkg';
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };

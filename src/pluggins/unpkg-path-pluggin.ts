646823d4c062fd8297045a69572d8b318e2ca11e
Brian Ho <YWHo@users.noreply.github.com>
2023-10-29 00:49:03 +1300

    using 'define' and package versioning

--- src/pluggins/unpkg-path-pluggin.ts
+++ src/pluggins/unpkg-path-pluggin.ts
@@ -31,8 +31,8 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              import React, { useState } from 'react@16.0.0';
+              console.log(React, useState);
             `,
           };
         }

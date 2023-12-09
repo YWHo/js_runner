97f1e19db9a9462495fb8aa71f038d96c6a8f0e2
Brian Ho <YWHo@users.noreply.github.com>
2023-12-10 00:15:38 +1300

    passing code to IFrame

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useRef, useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './pluggins/unpkg-path-pluggin';
-import { fetchPlugin } from './pluggins/fetch-plugin';
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom/client";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./pluggins/unpkg-path-pluggin";
+import { fetchPlugin } from "./pluggins/fetch-plugin";
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
   const ref = useRef<any>();
+  const iframeRef = useRef<any>();
+  const [input, setInput] = useState("");
+  // const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL:  `https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm`
+      wasmURL: `https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm`,
     });
   };
 
@@ -24,35 +25,58 @@ const App = () => {
     if (!ref.current) return;
 
     const result = await ref.current.build({
-      entryPoints: ['index.js'],
+      entryPoints: ["index.js"],
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframeRef.current.contentWindow.postMessage(
+      result.outputFiles[0].text,
+      "*"
+    );
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data); 
+            } catch (err) {
+              console.log(err);
+            }
+          }, false)
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
       <textarea
         value={input}
-        onChange={e => setInput(e.target.value)}
+        onChange={(e) => setInput(e.target.value)}
       ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      {/* <pre>{code}</pre> */}
+      <iframe ref={iframeRef} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(

6af4acc138537d9b9a5e7f2e07380765ea25deba
Brian Ho <YWHo@users.noreply.github.com>
2023-12-31 15:34:41 +1300

    refactoring: extract bundler code to a new folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,34 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import * as esbuild from 'esbuild-wasm';
 import Preview from './components/preview';
-import { unpkgPathPlugin } from './pluggins/unpkg-path-pluggin';
-import { fetchPlugin } from './pluggins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import bundle from './bundler';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
+const initialValue =
+  'const a = 1;\n' +
+  'const App = () => {\n' +
+  '  return (\n' +
+  '    <div>\n' +
+  '      <h1>Hello World</h1>\n' +
+  '      <button onClick={() => console.log("clicked!")}>Click me</button>\n' +
+  '    </div>\n' +
+  '  )\n' +
+  '}';
+
 const App = () => {
-  const ref = useRef<any>();
   const [userCode, setUserCode] = useState('');
-  const [input, setInput] = useState('');
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: `https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm`,
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
+  const [input, setInput] = useState(initialValue);
 
   const onClick = async () => {
-    if (!ref.current) return;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': 'production',
-        global: 'window',
-      },
-    });
-
+    const result = await bundle(input);
     setUserCode(result);
   };
 
   return (
     <div>
       <CodeEditor
-        initialValue={
-          'const a = 1;\n' +
-          'const App = () => {\n' +
-          '  return (\n' +
-          '    <div>\n' +
-          '      <h1>Hello World</h1>\n' +
-          '      <button onClick={() => console.log("clicked!")}>Click me</button>\n' +
-          '    </div>\n' +
-          '  )\n' +
-          '}'
-        }
+        initialValue={initialValue}
         onChange={(value) => setInput(value)}
       />
       <div>

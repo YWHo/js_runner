cebb6aa3132a1df41164f9fa410276ae9259b0f3
Brian Ho <YWHo@users.noreply.github.com>
2024-01-09 03:13:58 +1300

    minor update

--- craco.config.js
+++ craco.config.js
@@ -10,7 +10,7 @@ module.exports = {
             warning.module.resource.includes('node_modules') &&
             warning.details &&
             warning.details.includes('source-map-loader')
-          )
+          );
         },
       ],
       resolve: {

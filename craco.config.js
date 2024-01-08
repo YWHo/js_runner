bbdc31c59445309377282bee50b8ce90f56c5953
Brian Ho <YWHo@users.noreply.github.com>
2024-01-09 01:34:27 +1300

    replace 'react-app-rewired' to 'craco' and ignore sourcemap warnings

---
+++ craco.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: {
+    configure: {
+      ignoreWarnings: [
+        function ignoreSourcemapsloaderWarnings(warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes('node_modules') &&
+            warning.details &&
+            warning.details.includes('source-map-loader')
+          )
+        },
+      ],
+      resolve: {
+        fallback: {
+          buffer: require.resolve('buffer'),
+        },
+      },
+    },
+    plugins: {
+      add: [
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      ],
+    },
+  },
+};

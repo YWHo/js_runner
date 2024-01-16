276207b354fb0e9a1e869c5560eacbe5020d63fe
Brian Ho <YWHo@users.noreply.github.com>
2024-01-17 12:23:04 +1300

    minor styling updata

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -3,6 +3,7 @@ import { useTypedSelector } from '../hooks/use-typed-selector';
 import { memoizedSelectOrderedCells } from '../state/selectors';
 import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
+import './cell-list.css';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector((state) => memoizedSelectOrderedCells(state));
@@ -15,7 +16,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className='cell-list'>
       <AddCell previousCellId={null} forceVisible={cells.length === 0} />
       {renderedCells}
     </div>

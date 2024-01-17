99ab0024bffb8371151ea087477d9579b8cd15b4
Brian Ho <YWHo@users.noreply.github.com>
2024-01-17 14:15:35 +1300

    show an instruction on markdown on first loading

--- src/state/store.ts
+++ src/state/store.ts
@@ -9,27 +9,42 @@ export const store = legacy_createStore(
   composeWithDevTools(applyMiddleware(reduxThunk)),
 );
 
-const sampleCode =
-  '// This is a sample code. Please remove the comment below to try\n\n' +
-  '/*\n' +
-  'import React, { useState } from "react";\n' +
-  'import ReactDOM from "react-dom/client";\n' +
-  '' +
-  'const App = () => {\n' +
-  '  const [count, setCount] = useState(0);\n' +
-  '  console.log("count = ", count);\n' +
-  '' +
-  '  return (\n' +
-  '    <div>\n' +
-  '      <h1>This is a demo</h1>\n' +
-  '      <button onClick={() => setCount(count + 1)}>\n' +
-  '        Click count: {count}\n' +
-  '      </button>\n' +
-  '    </div>\n' +
-  '  )\n' +
-  '};\n' +
-  'show(<App />);\n' +
-  '*/';
+// Note: The following code is not supposed to be included in the store.ts file
+//       in actual production. This code is intended to produce a demo when the app
+//       is first launched so that users will understand what to do with the app.
+
+const sampleCode = `// This is a sample Javascript code.
+// To try out the sample, please remove the comment below ("/*" and "*/").
+
+/*
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  console.log("count = ", count);
+
+  return (
+    <div>
+      <h1>This is a demo</h1>
+      <button onClick={() => setCount(count + 1)}>
+        Click count: {count}
+      </button>
+    </div>
+  )
+};
+show(<App />);
+*/
+`;
+
+const instructionText = `This is a **markdown** editor.
+
+To edit the content, please click on this window.
+
+To add more windows, hover over the top or bottom border of any window to reveal buttons. After that, you can click on the "+ Code" or "+ Text" button.
+
+Please note that everything in this app is temporary. Your writing or code will be lost if you refresh or close the browser.
+`;
 
 store.dispatch({
   type: ActionType.INSERT_CELL_AFTER,
@@ -45,5 +60,6 @@ store.dispatch({
   payload: {
     id: null,
     type: 'text',
+    content: instructionText,
   },
 });
